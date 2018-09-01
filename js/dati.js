1df3b68124d44a05fa89578fffc1adfd391d06dc
marmat91 <marmat91@hotmail.it>
2018-09-01 20:46:02 +0200

    Update

--- js/dati.js
+++ js/dati.js
@@ -1,6 +1,26 @@
 // load the data
 d3.json("data/graph.json", function(data) {
     console.log(data);
+    d3.json("data/graph1.json", function(data1) {
+        console.log(data1);
+        var expenses=data1;
+
+        var cf	=	crossfilter(data1);
+        var gate = cf.dimension(function(d){return d.gatename});
+        gate.filter("entrance3");
+        console.log("num reports (entrance3)",cf.groupAll().reduceCount().value());
+
+        //	select VesselType,	count(*)	from	migrants group	by	VesselType
+        var countgate = gate.group().reduceCount();
+        console.log(countgate.all());
+        var alfa = countgate.all();
+
+        /*    var conta = d3.nest()
+                .key(function(d) { return d.gatename; })
+                .rollup(function(v)	{ return d3.mean(v,
+                    function(d)	{ return d.carid; });})
+                .entries(expenses);
+            console.log(conta);})*/
 /*    var circles = svg.append("g").selectAll("circle")
         .data(data.nodes)
         .enter()
@@ -17,7 +37,7 @@ d3.json("data/graph.json", function(data) {
         .selectAll("line")
         .data(data.links)
         .enter().append('line')
-        .attr('stroke-width', 1)
+        .attr('stroke-width', 0.2)
         .attr('stroke', '#E5E5E5');
 
 
@@ -28,11 +48,21 @@ d3.json("data/graph.json", function(data) {
         .enter().append("circle")
         .attr("fill", function(d) { return (d.color); })
 
+    // set scale for node size
+    var nodeSizeRange = [2, 7];
+    var minmax = d3.extent(alfa, function(d) { return d.value });
+    var nodeScale = d3.scaleLinear()
+        .range(nodeSizeRange)
+        .domain(minmax);
+    console.log(d3.merge([data.nodes, alfa]));
+
     var simulation = d3.forceSimulation()
-        .force("link", d3.forceLink().id(function(d) { return d.id; }))
+        .force("link", d3.forceLink().id(function(d) {return d.key;}))
+      //  .force("x", d3.forceX())
+       // .force("y", d3.forceY())
 
     node.append("title")
-        .text(function(d) { return d.id; });
+        .text(function(d) { return d.key; });
     // add nodes and links to simulation
     simulation
         .nodes(data.nodes)
@@ -54,15 +84,15 @@ d3.json("data/graph.json", function(data) {
             .attr("y2", function (d) { return d.target.ypos; });
 
         node
-            .attr("id", function(d) { return d.id; })
-            .attr("r", function(d) { return 2})
+            .attr("key", function(d) { return d.key })
+            .attr("r",function(d) { return nodeScale(10)})
             .attr("cx", function (d) { return d.xpos})
             .attr("cy", function (d) { return d.ypos})
     }
 
    // simulation.force('link').link(links)
 
-
+    })
 
 })
 
