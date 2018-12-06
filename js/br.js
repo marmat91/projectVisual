5114cfbb846838168acace3c6ea1261ad3d2d04e
marmat91 <marmat91@hotmail.it>
2018-12-06 19:48:00 +0100

    Update

--- js/br.js
+++ js/br.js
@@ -53,30 +53,43 @@ function provaBarre (data1){
     }
     data=a;
 
-
     var margin = {top: 20, right: 55, bottom: 30, left: 40},
         width  = 1000 - margin.left - margin.right,
         height = 500  - margin.top  - margin.bottom;
+
+    var svg_line = d3.select("#ch")
+        .attr("width",  width  + margin.left + margin.right)
+        .attr("height", height + margin.top  + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
     var x = d3.scaleBand ()
         .rangeRound([0, width])
         .padding(0.1);
+
     var y = d3.scaleLinear()
         .rangeRound([height, 0]);
+
+    var xAxis = d3.axisBottom(x);
+
+    var yAxis = d3.axisLeft(y);
+
+
+
     var line = d3.line()
         .x(function (d) { return x(d.label) + x.bandwidth() / 2; })
         .y(function (d) { return y(d.value); })
         .curve(d3.curveCardinal);
     ;
 
-    var color = d3.scaleBand()
-        .range(["#001c9c","#101b4d","#475003","#9c8305","#d3c47c","#6ed349","#6ed349"]);
-    console.log(a)
     var labelVar = 'quarter'; //A
+
     var varNames = d3.keys(a[0])
         .filter(function (key) { return key !== labelVar;}); //B
 
-    color.domain(varNames); //C
-    console.log(varNames)
+    var colore = { "Auto a 2 assi (o moto)":"#001c9c", "Camion a 2 assi":"#101b4d","Camion a 2 assi del parco":"#475003",  "Camion a 3 assi":"#9c8305",
+        "Camion 4 assi o superiore":"#d3c47c","Bus a 2 assi":"#6ed349", "Bus a 3 assi":"#6ed349" };
+
     var seriesData = varNames.map(function (name) { //D
         return {
             name: name,
@@ -86,8 +99,7 @@ function provaBarre (data1){
         };
     });
     console.log("seriesData", seriesData);
-    console.log(color.name)
-    console.log(color.domain)
+    console.log(colore);
 
     x.domain(data.map(function (d) { return d.quarter; })); //E
     y.domain([
@@ -99,19 +111,63 @@ function provaBarre (data1){
         })
     ]);
 
-    var series = d3.select("#ch").selectAll(".series")
+    svg_line.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+    svg_line.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Number of Rounds");
+
+
+    var series = svg_line.selectAll(".series")
         .data(seriesData)
         .enter()
         .append("g")
         .attr("class", "series");
-
     series.append("path")
         .attr("class", "line")
         .attr("d", function (d) { return line(d.values); })
-        .style("stroke", function (d) { console.log( color(d.name))
-            return color(d.name); })
+        .style("stroke", function (d) { return colore[d.name]; })
         .style("stroke-width", "4px")
         .style("fill", "none");
+
+    series.selectAll(".point")
+        .data(function (d) { return d.values; })
+        .enter().append("circle")
+        .attr("class", "point")
+        .attr("cx", function (d) { return x(d.label) + x.bandwidth()/2; })
+        .attr("cy", function (d) { return y(d.value); })
+        .attr("r", "5px")
+        .style("fill", function (d) { return colore[d.name]; })
+        .style("stroke", "grey")
+        .style("stroke-width", "2px")
+        .append("title")
+        .text(function(d) { return  d.name+"\nData: "+d.label + "\nNumero  " + d.value  ; })
+
+    var legend = svg_line.selectAll(".legend")
+        .data(varNames.slice().reverse())
+        .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function (d, i) { return "translate(55," + i * 20 + ")"; });
+    legend.append("rect")
+        .attr("x", width - 10)
+        .attr("width", 10)
+        .attr("height", 10)
+        .style("fill", function (d) {return colore[d]; })
+        .style("stroke", "grey");
+    legend.append("text")
+        .attr("x", width - 12)
+        .attr("y", 6)
+        .attr("dy", ".35em")
+        .style("text-anchor", "end")
+        .text(function (d) { return d; });
 
 
 
